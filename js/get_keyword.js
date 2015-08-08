ee2d81a583e4fb783c61896d87f4002af75e8ec8
Sana Oshika <sana@bigshika.com>
2015-08-09 09:38:57 +1200

    Added Javascript to return a random keyword from a hardcoded JSON list of Likes.

---
+++ js/get_keyword.js
@@ -0,0 +1,330 @@
+var jsonLikesList = {
+  "data": [
+    {
+      "id": "731197676989916",
+      "name": "SESA - Software Engineering Students Association",
+      "category": "Organization"
+    },
+    {
+      "id": "116301708405331",
+      "name": "Whittaker's Chocolate Lovers",
+      "category": "Food/Beverages"
+    },
+    {
+      "id": "531681433619507",
+      "name": "University Challenge NZ",
+      "category": "TV Show"
+    },
+    {
+      "id": "847751438594416",
+      "name": "Go.Women",
+      "category": "Society/Culture Website"
+    },
+    {
+      "id": "1376927319288555",
+      "name": "Food Culture",
+      "category": "Community"
+    },
+    {
+      "id": "104129093050548",
+      "name": "Google at UoA",
+      "category": "Education"
+    },
+    {
+      "id": "569105343121749",
+      "name": "Rails Girls Wellington",
+      "category": "Community Organization"
+    },
+    {
+      "id": "319059351608139",
+      "name": "Bottles & Beauty",
+      "category": "Personal Blog"
+    },
+    {
+      "id": "30382852317",
+      "name": "成龍 Jackie Chan",
+      "category": "Actor/Director"
+    },
+    {
+      "id": "212505698805486",
+      "name": "Oliver Sacks",
+      "category": "Author"
+    },
+    {
+      "id": "1400298830219237",
+      "name": "What We Do in the Shadows",
+      "category": "Movie"
+    },
+    {
+      "id": "116167105064723",
+      "name": "Summer of Tech",
+      "category": "Community"
+    },
+    {
+      "id": "453888271300676",
+      "name": "We Are Better Than That",
+      "category": "Community"
+    },
+    {
+      "id": "145839852284910",
+      "name": "CrackerJack Games .LLC",
+      "category": "Computers/Technology"
+    },
+    {
+      "id": "248480688605432",
+      "name": "Welcome to Night Vale",
+      "category": "Radio Station"
+    },
+    {
+      "id": "149264868600",
+      "name": "Ian McKellen",
+      "category": "Actor/Director"
+    },
+    {
+      "id": "191826274298779",
+      "name": "The Ramen Shop",
+      "category": "Restaurant/Cafe"
+    },
+    {
+      "id": "159403360903764",
+      "name": "VUW Tuataras",
+      "category": "Monarch"
+    },
+    {
+      "id": "187655488035282",
+      "name": "English Disco Lovers (EDL)",
+      "category": "Community"
+    },
+    {
+      "id": "119441931534072",
+      "name": "Tommy Millions",
+      "category": "Local Business"
+    },
+    {
+      "id": "606285079404173",
+      "name": "Action for Abortion Rights - Victoria University Wellington",
+      "category": "Community"
+    },
+    {
+      "id": "128916600997",
+      "name": "Embassy Theatre",
+      "category": "Movie Theater"
+    },
+    {
+      "id": "435618559822242",
+      "name": "Lgbti Asian Stories NZ",
+      "category": "Community"
+    },
+    {
+      "id": "209979102356180",
+      "name": "Griffin's NZ",
+      "category": "Food/Beverages"
+    },
+    {
+      "id": "128529000519949",
+      "name": "Emily de Molly",
+      "category": "Health/Beauty"
+    },
+    {
+      "id": "6502409274",
+      "name": "St Matthew-in-the-City",
+      "category": "Church/Religious Organization"
+    },
+    {
+      "id": "14621297389",
+      "name": "grabaseat",
+      "category": "Local/Travel Website"
+    },
+    {
+      "id": "132542320123923",
+      "name": "Pizza Pomodoro",
+      "category": "Local Business"
+    },
+    {
+      "id": "206293252750873",
+      "name": "The Critter Room",
+      "category": "Personal Blog"
+    },
+    {
+      "id": "119083341498319",
+      "name": "adc~",
+      "category": "Musician/Band"
+    },
+    {
+      "id": "240468359403185",
+      "name": "Victoria University of Wellington Library",
+      "category": "Education"
+    },
+    {
+      "id": "207349746059033",
+      "name": "Condescending Corporate Brand Page",
+      "category": "Community"
+    },
+    {
+      "id": "61765053139",
+      "name": "Spiderweb Software",
+      "category": "Software"
+    },
+    {
+      "id": "121255121263267",
+      "name": "James Gurney",
+      "category": "Artist"
+    },
+    {
+      "id": "127018160649520",
+      "name": "The Official Wellington SPCA",
+      "category": "Non-Profit Organization"
+    },
+    {
+      "id": "118984448122341",
+      "name": "Absolutely Positively Wellington",
+      "category": "Travel/Leisure"
+    },
+    {
+      "id": "133606026741381",
+      "name": "Max Factor New Zealand",
+      "category": "Health/Beauty"
+    },
+    {
+      "id": "394819907252384",
+      "name": "TATSUSHI",
+      "category": "Restaurant/Cafe"
+    },
+    {
+      "id": "359426880805298",
+      "name": "Campaign for Marriage Equality New Zealand",
+      "category": "Community"
+    },
+    {
+      "id": "259259914192799",
+      "name": "Protect NZ from 'Protect Marriage NZ'",
+      "category": "Community"
+    },
+    {
+      "id": "325354850850190",
+      "name": "Leesie Jane's Polish",
+      "category": "Health/Beauty"
+    },
+    {
+      "id": "145588134920",
+      "name": "White Ribbon New Zealand",
+      "category": "Cause"
+    },
+    {
+      "id": "197622310359815",
+      "name": "Campaign against changes to student allowances",
+      "category": "Community"
+    },
+    {
+      "id": "166799066707076",
+      "name": "Sam Rawlings Artist",
+      "category": "Artist"
+    },
+    {
+      "id": "195481013822322",
+      "name": "EthnicA",
+      "category": "Government Organization"
+    },
+    {
+      "id": "136917079665365",
+      "name": "Student Job Search",
+      "category": "Non-Profit Organization"
+    },
+    {
+      "id": "48725763533",
+      "name": "Issues.co.nz",
+      "category": "Local Business"
+    },
+    {
+      "id": "315138811329",
+      "name": "Centre for Applied Cross-Cultural Research",
+      "category": "University"
+    },
+    {
+      "id": "285779926080",
+      "name": "VUWSA - Victoria University of Wellington Students' Association",
+      "category": "Organization"
+    },
+    {
+      "id": "135959263100228",
+      "name": "Echo Bazaar",
+      "category": "App Page"
+    },
+    {
+      "id": "131580600211498",
+      "name": "Victoria University Press",
+      "category": "Local Business"
+    },
+    {
+      "id": "40384539979",
+      "name": "Aro Video",
+      "category": "Entertainment Website"
+    },
+    {
+      "id": "57642309048",
+      "name": "Wellington Batucada",
+      "category": "Musician/Band"
+    },
+    {
+      "id": "203985835428",
+      "name": "Extra Ordinary Comics",
+      "category": "Artist"
+    },
+    {
+      "id": "157667035655",
+      "name": "New Zealand Opera",
+      "category": "Non-Profit Organization"
+    },
+    {
+      "id": "130268057061988",
+      "name": "LegaliseLove",
+      "category": "Community"
+    },
+    {
+      "id": "218912961455140",
+      "name": "New Zealand International Film Festival",
+      "category": "Non-Profit Organization"
+    },
+    {
+      "id": "201412066560080",
+      "name": "Nadia Lim",
+      "category": "Public Figure"
+    },
+    {
+      "id": "172168074533",
+      "name": "Wellington Film Society",
+      "category": "Non-Profit Organization"
+    },
+    {
+      "id": "1385371715035391",
+      "name": "Assassin's Creed",
+      "category": "Games/Toys"
+    },
+    {
+      "id": "274970656197",
+      "name": "Hong Kong Subtitles",
+      "category": "Entertainment Website"
+    },
+    {
+      "id": "323605276025",
+      "name": "The Remarkable Sweet Shop",
+      "category": "Shopping/Retail"
+    },
+    {
+      "id": "75371167746",
+      "name": "Skateistan",
+      "category": "Non-Governmental Organization (NGO)"
+    },
+    {
+      "id": "32009667053",
+      "name": "Mighty Ape",
+      "category": "Retail and Consumer Merchandise"
+    }]
+}
+function get_keyword(friendId) {
+  var listOfLikes =  jsonLikesList.data;
+  console.log(listOfLikes);
+  var randomNumber = Math.round(Math.random() * (listOfLikes.length - 1));
+  console.log(listOfLikes[randomNumber]);
+  return listOfLikes[randomNumber].name;
+}
